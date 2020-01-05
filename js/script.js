662547bf96a09b074055141c9539c7a183fff924
viktr 666 <49025707+viktr1432@users.noreply.github.com>
2020-01-05 23:03:26 +0300

    ++

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
 // slick-slider
-$('.info-slider').slick({ 
-    autoplay: true,
+$('.info-slider').slick({
+    autoplay: false,
 });
-$('.product-slider_items').slick({ 
-    autoplay: true,
+$('.product-slider_items').slick({
+    autoplay: false,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 320,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
 });
+// menu -mobile
+$(document).ready(function(){
+    $('.burger-menu').click(function(){
+        $('.burger-menu,.header-menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    })
+})
